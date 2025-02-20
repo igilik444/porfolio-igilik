e62aa7a71431e6e010840b17cd92615bdb213a8b
igilik444 <igilik.tinimbai@gmail.com>
2025-02-20 12:20:10 +0500

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 document.getElementById("language-toggle").addEventListener("click", function() {
     let aboutText = document.getElementById("about-text");
-    if (aboutText.innerText === "Я занимаюсь созданием крутых мобильных видео и постоянно развиваюсь в этой сфере.") {
+    let currentText = aboutText.innerText;
+
+    if (currentText === "Я занимаюсь созданием крутых мобильных видео и постоянно развиваюсь в этой сфере.") {
+        aboutText.innerText = "Мен мобильді бейнелер жасаумен айналысамын және осы салада үнемі дамып келемін.";
+    } else if (currentText === "Мен мобильді бейнелер жасаумен айналысамын және осы салада үнемі дамып келемін.") {
         aboutText.innerText = "I create cool mobile videos and constantly improve in this field.";
     } else {
         aboutText.innerText = "Я занимаюсь созданием крутых мобильных видео и постоянно развиваюсь в этой сфере.";
